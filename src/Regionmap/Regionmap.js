33e020168dcfd1b75ca4dc9bf90fe20618eebebb
Denis Tokarev <blok1993@yandex.ru>
2018-05-12 13:31:08 +0300

    feat: RegionModule API ready

--- src/Regionmap/Regionmap.js
+++ src/Regionmap/Regionmap.js
@@ -1,14 +1,33 @@
+import 'mapsapi-polygonmap/src/Polygonmap/Polygonmap';
+
 ymaps.modules.define('Regionmap', [
-    'option.Manager'
-], (provide, OptionManager) => {
+    'option.Manager',
+    'Polygonmap'
+], (provide, OptionManager, Polygonmap) => {
     class Regionmap {
-        construcotor(data, options) {
+        constructor(data, options) {
             this._data = data;
             this.options = new OptionManager(options);
+
+            this._prepare(this.options.get('regionOptions'));
         }
 
         setMap(map) {
             this._map = map;
+        }
+
+        getMap() {
+            return this._map;
+        }
+
+        _prepare(params) {
+            return ymaps.borders.load(params.region, params)
+                .then((data) => this._render(data), this);
+        }
+
+        _render(data) {
+            const polygonmap = new Polygonmap({polygons: data, points: this._data});
+            polygonmap.setMap(this.getMap());
         }
     }
 
